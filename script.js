031b58eba8f125d7fbcce378f897722a18bee107
Gyuman Kim <kgmblue9@gmail.com>
2024-02-18 13:52:36 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+//배열함수
+
+let names = ["Steven Paul Jobs",
+"Bill Gates",
+"Mark Elliot Zuckerberg",
+"Elon Musk",
+"Jeff Bezos",
+"Warren Edward Buffett",
+"Larry Page",
+"Larry Ellison",
+"Lloyd Blankfein"
+]
+
+// map 문제
+// * 모든 이름을 대문자로 바꾸어서 출력하시오.
+// * 성을 제외한 이름만 출력하시오.(예 ["Steven", "Bill", "Mark"...])
+// * 이름의 이니셜만 출력하시오. (예 ["SPJ", "BG", "MEZ"...])
+
+let upperName = names.map((item)=>{
+    return item.toUpperCase()
+})
+console.log(upperName);
+
+let firstName = names.map((item)=>{
+    return item.
+})
+console.log(firstName);
